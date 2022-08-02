f59a2653f6da7b272e5fb348dd8f753af7d7fbd3
Colin Wolfe <Coho905@gmail.com>
2022-08-01 23:12:51 -0400

    added the subset function

--- backendfunctions.cpp
+++ backendfunctions.cpp
@@ -62,3 +62,40 @@ bool subset(A, B, datatype){
 	return final; // will return true if the set of elements in A but not B is empty, and the set of elements in B but not A is not empty
 }
 
+
+//is_empty_set() call
+/*
+Assuming A is a properly declared vector 
+Returning a boolean on if a set is equivalent to the empty set
+*/
+bool is_empty_set(A){
+	bool empty = A.size()==0;
+	return empty;
+	// If we really wanted to get complicated and computationally stupid we could check to see what the difference is for each and see if it is both 0 but we aren't that dumb ;)
+}
+
+
+
+
+// THE DIFFERENCE OPERATION
+/*
+A \ B
+Assuming A is a properly declared vector and B is a properly declared vector filled with datatypes that match A
+Returns a vector of elements in A that are not in B
+*/
+template <typename T> 
+vector<T> difference(A, B){
+	vector<T> vect_inA_notB;
+	for(T element: A){
+		bool found = false;
+		for(T element2: B){
+			if(element==element2){
+				found = true;
+			}
+		}
+		if(!found){
+			vect_inA_notB.push_back(element);
+		}
+	}
+	return vect_inA_notB;
+}
