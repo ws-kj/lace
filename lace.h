a81ec378ab345cdb31727ae9452843df0e991d00
Will Savage <wsavage6316@gmail.com>
2022-08-01 23:20:42 -0400

    add basic Set

---
+++ lace.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace lace 
+{
+	typedef double Num;
+
+	class Set 
+	{
+		std::string name;
+		std::vector<lace::Num> data;
+
+	public:
+		Set();
+		Set(std::vector<lace::Num>& vec);
+		Set(std::string name);
+		Set(std::vector<lace::Num>& vec, std::string name);
+		
+		std::string getName();
+		std::vector<lace::Num> asVec();
+	};
+
+	template<class T>
+	class Result {
+
+	};
+}
